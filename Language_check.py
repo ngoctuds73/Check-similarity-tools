0df9a24f9b2b87cac2b76755f41fcb1eb10cad83
ngoctuds73 <123917622+ngoctuds73@users.noreply.github.com>
2023-03-20 11:21:33 +0700

    Update Language_check.py

--- Language_check.py
+++ Language_check.py
@@ -1,42 +1,43 @@
 class ProgrammingLanguage:
-    def __init__(self, name, file_extension):
-        self.name = name
-        self.file_extension = file_extension
+    class ProgrammingLanguage:
+        def __init__(self, name):
+            self.name = name
 
-    def get_name(self):
-        return self.name
-
-    def set_name(self, name):
-        self.name = name
-
-    def get_file_extension(self):
-        return self.file_extension
-
-    def set_file_extension(self, file_extension):
-        self.file_extension = file_extension
-
-    def __str__(self):
-        return f"{self.name}.{self.file_extension}"
+        def get_file_extension(self):
+            pass
 
 
-class ProgrammingLanguageChecker:
-    def __init__(self, filename):
-        self.filename = filename
+class PythonLanguage(ProgrammingLanguage):
+    def __init__(self):
+        super().__init__('Python')
 
-    def get_file_extension(self):
-        return self.filename
+    @staticmethod
+    def get_file_extension():
+        return '.py'
 
-    def check_language(self):
-        ext = self.get_file_extension()
-        if ext == "py":
-            return ProgrammingLanguage("Python", ext)
-        elif ext == "js":
-            return ProgrammingLanguage("JavaScript", ext)
-        elif ext == "java":
-            return ProgrammingLanguage("Java", ext)
-        elif ext == "cpp" or ext == "cc" or ext == "cxx":
-            return ProgrammingLanguage("C++", ext)
-        elif ext == "c":
-            return ProgrammingLanguage("C", ext)
-        else:
-            return None
+
+class JavaLanguage(ProgrammingLanguage):
+    def __init__(self):
+        super().__init__('Java')
+
+    @staticmethod
+    def get_file_extension():
+        return '.java'
+
+
+class CLanguage(ProgrammingLanguage):
+    def __init__(self):
+        super().__init__('C')
+
+    @staticmethod
+    def get_file_extension():
+        return '.c'
+
+
+class CppLanguage(ProgrammingLanguage):
+    def __init__(self):
+        super().__init__('C++')
+
+    @staticmethod
+    def get_file_extension():
+        return '.cpp'
